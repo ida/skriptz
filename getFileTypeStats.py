cbe6f7b10fddd2b333fedbb2959e08d414890859
Ida Ebkes <contact@ida-ebkes.eu>
2014-07-25 12:20:13 +0200

    Update getFileTypeStats.py

--- getFileTypeStats.py
+++ getFileTypeStats.py
@@ -1,29 +1,32 @@
 # !/usr/bin/python
 
-# Get some stats about file-types recursively of the directory where you execute:
-# python getFileTypeStats.py
+# Get files which need i18n for doublecheck.
 
 import os
 
-wanted_file_types = ['.js', '.py','.pt','.cpt', '.xml', '.zpt', '.cpy', '.vpy']
+pys = ['.py', '.cpy', '.vpy']
+pts = ['.pt', '.cpt', '.zpt']
+# needs our own translation-package:
+xml_own = ['actions','controlpanel','type']
+# needs another translation-package:
+xml_plone = ['portlets', 'portal_atct']
 
-unwanted_file_types = ['.dtml', '.patch', '.props', '.robot', '.ZPL', '.yml', '.c', '.pyc','.png','.txt', '.zcml', '.css', '.GPL', '.csv', '.in', '.gif', '.jpg', '.cfg', '.mo', '.po', '.pot', '.sample', '.rst', '.pack', '.idx', '.stx', '.swp', '.avi', '.ini', '.metadata', '.ico', '.3gp', '.py_tmpl', '.ini_tmpl', '.bat']
-
-defined_file_types = wanted_file_types + unwanted_file_types
-
-py_fy = 0
-pg_tm = 0
-zpgtm = 0
+exclude_pys = ['merge.py', 'setup.py', 'config.py', 'setuphandlers.py', 'subscribers.py']
+# TODO: recheck if config.py is needed
+pies  = 0
+tmpl  = 0
 xml   = 0
-js    = 0
-to_ch = 0
+ecma  = 0
 summ  = 0
+total = 0
 
 # Walk through directory recursively:
 for root, dirs, files in os.walk("."):
 
     # For each file:
     for file_name in files:
+
+        total += 1
 
         file_path = os.path.join(root, file_name)
 
@@ -33,28 +36,40 @@ for root, dirs, files in os.walk("."):
 
             suff = splitted_name[1]
 
-            if suff in wanted_file_types:
-                if suff == '.py':
-                    py_fy += 1
-                elif suff == '.zpt':
-                    zpgtm += 1
-                elif suff == '.pt':
-                    pg_tm += 1
-                elif suff == '.xml':
+            # *PY
+            if suff in pys:
+                if (file_path.find('/tests/') == -1):
+
+                    if file_name not in exclude_pys:
+                        isEmpty = True
+                        with open(file_path) as fil:
+                            line = fil.readline()
+                            while(line):
+                                if line[0] != '#' and line != '' and line != '\n':
+                                    isEmpty = False
+                                    foundApo = False
+                                    if ( (line.find('"""') == -1) or (line.find("'''") == -1) ) and ( (line.find('"') != -1) or (line.find("'") != -1) ):
+                                        if (line.find('Initializer') == -1) and (line.find("loadMigrationProfile") == -1) and (line.find("pkg_resources") == -1):
+                                            foundApo = True
+
+                                line = fil.readline()
+                        if not isEmpty and foundApo:
+                            pies += 1
+                            #print file_name
+            # *PT
+            elif suff in pts:
+                tmpl += 1
+
+            # XML
+            elif suff == '.xml' and file_path.find('profile') != -1:
+                if ( file_path.find('upgrades') == -1 and file_path.find('uninstall') == -1 and file_path.find('type') != -1 or file_name in (xml_own or xml_plone) ):
                     xml   += 1
-                elif suff == '.js':
-                    js    += 1
-                else:
-                    print suff
-                    to_ch += 1
 
-        else:
-            print 'No extension-suffix for: ' + splitted_name
+            # JS
+            elif suff == '.js':
+                ecma    += 1
 
 
-        if (  (suff not in defined_file_types) and (suff != '') ) :
-            print 'Unknown file-extension-suffix: ' + suff
+summ = pies + tmpl + xml + ecma
 
-summ = py_fy + pg_tm + zpgtm + xml + to_ch
-
-print 'We have ' + str(py_fy) + ' python-scripts, ' + str(pg_tm) + ' page-templates, ' + str(zpgtm) + ' zope-templates, ' + str(xml) + ' xml-configs, ' + str(js) +' javascripts and ' + str(to_ch)  + ' other (.cpt, .cpy, .vpy) to check. That is a total of ' + str(summ) + '.'
+print 'We have ' + str(pies) + ' python-scripts, ' + str(tmpl) + ' page-templates, '  + str(xml) + ' xml-configs, ' + str(ecma) +' javascripts to check. That is a total of ' + str(summ) + '.'
