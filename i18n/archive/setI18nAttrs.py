10129c8752a1ac2016f2a450189240384aa8337a
Ida Ebkes <contact@ida-ebkes.eu>
2014-08-10 01:36:33 +0200

    clean

--- i18n/archive/setI18nAttrs.py
+++ i18n/archive/setI18nAttrs.py
@@ -1,94 +1,91 @@
 # !/usr/bin/python
-
 import os
 import shutil
 from bs4 import BeautifulSoup
-
-msg_nm = 0
-msg_id = 0
-wanted_file_types = ['.pt']
+wanted_file_types = ['.pt', '.cpt', '.zpt']
 domain = 'amp.translations'
-
-# Walk through directory recursively:
 for root, dirs, files in os.walk("."):
-
-    # For each file:
     for file_name in files:
-
         file_path = os.path.join(root, file_name)
-
         splitted_name = os.path.splitext(file_name)
-
         if len(splitted_name) > 0:
-
             suff = splitted_name[1]
-
             if suff in wanted_file_types:
-
-                HAS_DOMAIN = False
-# We specify the parser to use to avoid mess-ups,
-# f.e., if lxml is installed it would be taken,
-# which we don't want because prettify() then
-# (see end of file) adds html- and body-tag,
-# let's use Python's built-in parser 'html.parser':
                 food = open(file_path)
                 soup = BeautifulSoup(food, 'html.parser')
                 tags = soup.find_all()
-
                 for tag in tags:
-
-                    #
-                    # i18n
-                    #
-                   
-                    # Needs i18n:attribute ? Tags with title, alt 
-                    # or value do, collect them:
-		    HAS_i18n_ATT = False
-		    need_i18n_att = []
+                    HAS_i18n_ATT = False
+                    need_i18n_att = []
                     dont_need_i18n_att = []
-		    attis = tag.attrs
-
+                    attis = tag.attrs
+                    # For each att in tag:
                     for atti in attis:
+                        
+                        # Collect title-, alt- or value-att:
                         if (atti == 'title') or (atti == 'alt') or (atti == 'value'):
-			    # Has a val:
-			    if len(tag[atti]) > 1:
-				# It's not a var:
-			    	if tag[atti][0] != '$'and tag[atti][1] != '{':
-			            need_i18n_att.append(atti)
+                        
+                            # Att has a val:
+                            if tag[atti] != '':
+                                
+                                # Val is not a var, otherwise ignore:
+                                if tag[atti][0] != '$':
+                                    
+                                    # Collect:
+                                    need_i18n_att.append(atti)
 
-			# Overriden by tal-att or i18n-att already set? Ignore them later:
-			if atti == ('i18n:attributes' or 'tal:attributes'):
-                            atts = tag[atti]
-                            atts = atts.split(';')
-                            for att in atts:
-                                att = att.strip()
-                                att = att.split(' ')[0]
-				dont_need_i18n_att.append(att)
-		    	    if atti == 'i18n:attributes':
-				HAS_i18n_ATT = True
+                        # Overriden by tal-att or i18n-att already set?
+                        # Collect to ignore them later:
+                        if atti == ('i18n:attributes' or 'tal:attributes'):
+                                        atts = tag[atti]
+                                        atts = atts.split(';')
+                                        for att in atts:
+                                            att = att.strip()
+                                            att = att.split(' ')[0]
+                                            # Collect:
+                                            dont_need_i18n_att.append(att)
+                        if atti == 'i18n:attributes':
+                            HAS_i18n_ATT = True
 
-		    # Compare and set i18n:attribute:
-		    string = ''
-		    for need in need_i18n_att:
-                	if need not in dont_need_i18n_att:
-			    string += ' ' + need + ';'
-		    if string != '':
-               	    	if HAS_i18n_ATT:
-			    string = tag['i18n:attributes'] + string
-		    	else:
-			    string = string.strip()
-			    tag['i18n:attributes'] = string 
-	        
-		if tag.name == 'input':
-		    HAS_TIT = False
-		    ats = tag.attrs
-		    for at in ats:
-			if at == 'value':
-			    HAS_TIT = True
-		    if HAS_TIT == False:
-			print tag
-			print file_path
+                        # End of 'for att in attis',
+                    
+                    # back to tag:
 
+                    ################################
+                    ################################
+                    
+                    
+                    ################################
+                    # Compare if needed, collect and set i18n:attribute:
+                    value = ''
+                    
+                    # Compare:
+                    for need in need_i18n_att:
+                            
+                            # Really needed?
+                            if need not in dont_need_i18n_att:
+                                
+                                # Collect val-str:
+                                value += ' ' + need + ';'
+                    
+                    # Collected val-str is not empty, 
+                    # we have sth to set later on:
+                    if value != '':
+
+                        # Already has i18n-att: 
+                        if HAS_i18n_ATT:
+                            # Add val to existing val:
+                            value = value + tag['i18n:attributes'] 
+                        
+                        # No i18n-att:
+                        else:
+                            value = value.strip()
+                            # Set att initially:
+                            tag['i18n:attributes'] = value 
+                
+                # EO for each tag    
+                
+                # Prepare soup and overwrite template with cooked i18n-hooks:
                 soup = soup.prettify().encode('ascii', 'xmlcharrefreplace')
                 result = open(file_path + '.tmp', 'w')
                 result.write(str(soup))
