ba92c7b7216ba7edf64f363a3a63e03f6aab6733
Ida Ebkes <contact@ida-ebkes.eu>
2014-08-12 19:09:37 +0200

    up

--- py/extractWordsBetweenQuotesOfFile.py
+++ py/extractWordsBetweenQuotesOfFile.py
@@ -1,68 +1,78 @@
-# extract words of between single quotes
-import re
-# define
-delis = ['"', "'"]
-deli = None
-words = []
-word = ''
-pos = -1
-WORD = False
-# feed
 food = open('bsp.py')
 string = food.read()
 food.close()
-TRIP = False
-count = 0
-# act
-while pos < len(string):
-    char = string[pos]
 
-    # We have a DELI:
-    if char in delis:
+def getWords(string):
+    """ Get words of between quotations.
+        Regard single-, double- and tripled-quotes.
+    """
+    count = 0
+    delis = ['"', "'"]
+    deli = None
+    words = []
+    word = ''
+    pos = -1
+    WORD = False
+    TRIP = False
+
+    while pos < len(string):
+        char = string[pos]
+
+        # We have a DELI:
+        if char in delis:
+            
+            # Detect triple delis by looking 
+            # for next two characters:
+            if len(string) > pos+1:
+                if string[pos+1] == deli:
+                    if len(string) > pos+2:
+                        if string[pos+2] == deli:
+                            if TRIP:
+                        # If word derived of a triple, we need to remove,
+                        # the first two characters, which are quots collected 
+                        # of the opening triple-quote-include-deli-loop:
+                                word = word[2:]
+                                TRIP = False
+                            else:
+                                TRIP = True
+                                deli = None
+            
+            
+            # Compare if same, as starting deli (if deli not None):
+            if deli == char:
+                
+                # Not in triple, word closes, ENDING DELI:
+                if not TRIP:
+                     
+                    # WORD ENDS:
+                    deli = None
+                
+                    # Collect word in list, ignore linebreaks:
+                    if word != '' and word != '\n':
+                        words.append(word)
+                    
+                    # Reset word:
+                    word = ''
+
+
+                # We are in triple, collect this deli-char in comment,
+                else:
+                    word += char
+            # If no deli set, set it, STARTING DELI:
+            if not deli:
+                deli = char
+        # Char is not a deli and we are not in a triple quoted word:
+        else:
+            if deli and not TRIP:
+            # A deli is set, we are in a word:
+            #if deli:
+                # Collect char:
+                word += char
+            # We are in triple, collect this char:
+            elif TRIP:
+                word += char
         
-        # Detect  triple delis:
-        if len(string) > pos+1:
-            if string[pos+1] == deli:
-                if len(string) > pos+2:
-                    if string[pos+2] == deli:
-                        count+=1
-                        print TRIP
-                        if TRIP: TRIP = False
-                        else: TRIP = True
-        
-        # Compare if same, as starting deli (if deli not None):
-        if deli == char:
-            
-            # Not triple and same as starting deli, word closes, ENDING DELI:
-            if not TRIP: deli = None
-            
-            # Collect word in list, ignore added linebreaks of collecting:
-            if word != '' and word != '\n':
-                words.append(word)
-                #print TRIP
-            
-            # Reset word:
-            word = ''
-
-
-        # If no deli set, set it, STARTING DELI:
-        if not deli:
-            deli = char
-    # Char is not a deli and we are not in a triple quoted word:
-    else:
-        if not TRIP and deli:
-        # A deli is set, we are in a word:
-        #if deli:
-            # Collect char:
-            word += char
-    
-    pos += 1
-
-
-
-print words
-
-print count
-
-
-# EOF
+        pos += 1
+    print words
+# RUN
+getWords(string)
