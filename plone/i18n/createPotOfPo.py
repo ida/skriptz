965d2f03d4793ea694456310c8bfd4dc74c6a60d
Ida Ebkes <contact@ida-ebkes.eu>
2014-11-19 08:29:20 +0100

    up

---
+++ plone/i18n/createPotOfPo.py
@@ -0,0 +1,10 @@
+import shutil
+source = 'amp.translations.pot'
+target = source + '.tmp'
+with open(source) as fin, open(target, 'w') as fout:
+    for line in fin:
+        if line.startswith('msgstr "'):
+            fout.write('msgstr ""\n')
+        else:
+            fout.write(line)
+    shutil.move(target, source)            
