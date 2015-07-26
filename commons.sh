bfe1cf7eac14eca1dff50c5246b081ac034fb3b4
Ida Ebkes <contact@ida-ebkes.eu>
2015-07-27 00:46:27 +0200

    up

--- commons.sh
+++ commons.sh
@@ -1,10 +1,17 @@
-showLastReturnedVal() { last_returned_val=$?; echo $last_returned_val; }
+# Reminders
+str_of_file=$( <path/to/file )
+last_returned_val=$?
 
+# Comparisons
 strEqualsStr () { if [ "$1" = "$2" ]; then return 1; else return 0; fi }
 strStartswithStr () { if [[ "$1" = "$2"* ]]; then return 1; else return 0; fi }
 strContainsStr () { if [[ "$1" = *"$2"* ]]; then return 1; else return 0; fi }
 strEndswithStr () { if [[ "$1" = *"$2" ]]; then return 1; else return 0; fi }
+# strEndswithStr 'The end.' 'end.'
+# if [[ $? = 1]]; then...........
 
-showFileStrOfPath() { echo $( <$1 ); }
+# Loop
+forEachDo() { for i in $1; do $2 $i; done }
+# forEachDo 'a b c' echo
 
-inEachFirstDirDo() { for file in *; do if [ -d $file ]; then cd $file; $1; cd ..; fi done; }
+
