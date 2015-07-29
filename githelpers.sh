fa1fb0e267bf2a81032f589e14234f084e0cc3e8
Ida Ebkes <contact@ida-ebkes.eu>
2015-07-29 15:01:38 +0200

    up

--- githelpers.sh
+++ githelpers.sh
@@ -3,6 +3,12 @@
 # Perform git-actions over several repos at once,
 # given, they live in the same directory.
 #
+# Produces reportfiles in this directory:
+unpushed_commits_report='git-unpushed-commits-report.txt'
+diff_report='git-diff-report.txt'
+
+#
+#
 # Assumes we are inside of one of a repos' 1st-level-dir,
 # and the other repos live in the same dir, as this one does.
 
@@ -10,21 +16,8 @@ strContainsStr () { if [[ "$1" = *"$2"* ]]; then return 1; else return 0; fi }
 fileIsEmpty () { if [[ $( <"$1" ) = '' ]]; then return 1; else return 0; fi; }
 inEachFirstDirDo() { for file in *; do if [ -d $file ]; then cd $file; $1; cd ..; fi done; }
 
-checkForDiffs() {
-# Checks for git-diffs in all repos living in the same directory
-# and creates a reportfile of it in the, one level above of where this
-# script is executed.
-    reportfile_name='git-diff-report.txt'
-    reportfile=$basket_lokus/$reportfile_name
-    cd $basket_lokus
-    inEachFirstDirDo 'git diff' > "$reportfile"
-    fileIsEmpty $reportfile
-#    if [[ $? == 1 ]]; then echo 'Everythings clean, no diffs :-)'; else echo Not clean, check: $reportfile; fi
-}
-
 checkForUnpushedCommits() {
-    reportfile_name='git-unpushed-commits-report.txt'
-    reportfile=$basket_lokus/$reportfile_name
+    reportfile=$basket_lokus/$unpushed_commits_report
     echo "
 The following repos have commits, waiting to be pushed:
 " > "$reportfile"
@@ -40,8 +33,16 @@ The following repos have commits, waiting to be pushed:
         fi
     done
 }
-#    inEachFirstDirDo 'git status' > "$reportfile"
-#    if [[ $? == 1 ]]; then echo 'Everythings clean, no diffs :-)'; else echo Got diffs, check: $reportfile; fi
+checkForDiffs() {
+# Checks for git-diffs in all repos living in the same directory
+# and creates a reportfile of it in the, one level above of where this
+# script is executed.
+    reportfile=$basket_lokus/$diff_report
+    cd $basket_lokus
+    inEachFirstDirDo 'git diff' > "$reportfile"
+    fileIsEmpty $reportfile
+    if [[ $? == 1 ]]; then echo 'Everythings clean, no diffs :-)'; else echo There are diffs, check: $reportfile; fi
+}
 
 main() {
     cd ..
