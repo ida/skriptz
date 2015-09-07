5ae411d99d92c801a26b5df1b212ad756c70078d
Ida Ebkes <contact@ida-ebkes.eu>
2015-09-07 08:05:16 +0200

    Update find_catalogued_broken_objs.py

--- py/plone/find_catalogued_broken_objs.py
+++ py/plone/find_catalogued_broken_objs.py
@@ -1,4 +1,6 @@
+
 # Find broken objs, to be able to recreate them, so reindexing or upgrading will work:
+# Usage: Via ZMI, locate to portal_skins/custom, select "Script(Python)" of dropdwonmenu, insert these lines, save. Then click "Test"-tab on top.
 from Products.CMFCore.utils import getToolByName
 catalog = getToolByName(context, 'portal_catalog')
 results = catalog.searchResults(Language='all')
@@ -8,3 +10,8 @@ for brain in results:
     except:
         print brain.getPath()
 return printed
+
+#TODO: Extend this script to bypass expired-content-permisson-check, to get *all* objs:
+#mtool = context.portal_membership
+#show_inactive = mtool.checkPermission('Access inactive portal content', context)
+#contents = context.portal_catalog.queryCatalog(show_inactive=show_inactive)
