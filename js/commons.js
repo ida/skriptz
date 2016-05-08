70ea18f57920ff08c431038bd56b7d6a13357e50
Ida Ebkes <contact@ida-ebkes.eu>
2016-05-08 20:44:52 +0200

    up

--- js/commons.js
+++ js/commons.js
@@ -31,7 +31,6 @@ Examples: addEle(some_ele, 'div', 'blabla')
   parent_ele.appendChild(ele)
   return ele
 }
-
 function getEle(ele) {
 // Promises: Passed 'ele' can be obj or id-name or tag-name, return ele-obj.
   if (typeof ele === 'string') {
@@ -39,24 +38,23 @@ function getEle(ele) {
   }
   return ele
 }
-
 function getNextSibling(ele) {
   var next_ele=ele.nextSibling
   while (next_ele.nodeType != 1) {
     next_ele=next_ele.nextSibling
   }
   return next_ele
-};
-
+}
 function getFirstChild(ele) {
   var first_child=ele.firstChild
   while (first_child.nodeType != 1) {
     first_child=getNextSibling(first_child)
   }
   return first_child
-};
-
-
+}
+function hasClass(ele, klassName) {
+  return (' ' + ele.className + ' ').indexOf(' ' + klassName + ' ') > -1
+}
 //
 // STYLES
 //
