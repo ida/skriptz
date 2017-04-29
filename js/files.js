053ef60862f033f2a2d2381f0dbe8d19aaa0c52b
Ida Ebkes <contact@ida-ebkes.eu>
2017-04-29 15:19:45 +0200

    Improve file.js.

--- js/files.js
+++ js/files.js
@@ -1,38 +1,46 @@
+// What
+// ====
+// Let a user select a file from their filesystem, immediately upload
+// it, and do something with its content.
+//
+// Why
+// ===
+// Provide a possibility to upload a file's content into a serverless
+// browser-app. For example to read csv-files and display them in a
+// table-like manner as html-lists.
+//
+// Usage
+// =====
+// function doStWithContentAfterUpload () { console.debug(content) }
+// var inputContainer = document.body
+// provideFileUpload(inputContainer, doStWithContentAfterUpload)
+//
+// Docs
+// ====
 // https://developer.mozilla.org/en-US/docs/Web/API/File
-
-function addFilesInputEle(containerEle) {
+//
+function addFilesInputEle(inputContainer) {
   var input = document.createElement('input')      // create input
   input.type = 'file'                      // make it a file-input
-  input.setAttribute('multiple', '')      // enable multiple files
-  containerEle.appendChild(input)           // insert input in DOM
+  input.setAttribute('multiple', 'true')  // enable multiple files
+  inputContainer.appendChild(input)         // insert input in DOM
   return input
 }
-function doAfterLoadWithFileContent(content) {
-document.body.innerHTML = content
-}
-function getSelectedFiles(inputEle) {
-  return inputEle.files
-}
-function loadFile(file, reader, doAfterLoadWithFileContent=null) {
-  if(doAfterLoadWithFileContent) {       // if function was passed
-    reader.onloadend = function(eve) {      // when a file is read
-        doAfterLoadWithFileContent(eve.target.result)  // exe func
-    }
-    // Let's use the read-method, which supports most browsers:
-    reader.readAsArrayBuffer(file)                    // load file
+function provideInputEle(inputContainer, reader) {
+  var input = addFilesInputEle(inputContainer)        // add input
+  input.onchange = function(eve) {      //  when user selects file
+    var file = eve.target.files[0]                  //    get file
+    reader.readAsText(file)                        //    load file
   }
 }
-function main() {
-  var reader = new FileReader()
-  var input = addFilesInputEle(document.body)
-  input.onchange = function(eve) {         // user selects file(s)
-    var files = getSelectedFiles(eve.target)
-    for(var i = 0; i < files.length-1; i++) {     // for each file
-       var file = files[i]                // load and process file
-       loadFile(file, reader, doAfterLoadWithFileContent)
-    }
+function provideFileReader(doAfterFileUpload=null) {
+  var reader = new FileReader()           //   provide file-reader
+  reader.onloadend = function(eve) {     // when a file has loaded
+    doAfterFileUpload(eve.target.result)// do sth with its content
   }
+  return reader
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-  main()
-});
+function provideFileUpload(inputContainer, doAfterFileUpload) {
+  var reader  = provideFileReader(doAfterFileUpload)
+  provideInputEle(inputContainer, reader)
+}
