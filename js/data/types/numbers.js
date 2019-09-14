cd901db845bed01580dedefb4b6b4fc1590a573c
Ida Ebkes <contact@ida-ebkes.eu>
2019-09-14 13:32:02 +0200

    Add secondsToMs()

--- js/data/types/numbers.js
+++ js/data/types/numbers.js
@@ -8,3 +8,32 @@ function isNr(value) {
   // Regard `Number` returns zero for empty strings.
   return value != '' && isNaN(Number(value)) === false
 }
+
+
+function secondsToMs(seconds) {
+
+// Return milliseconds with a length of exactly four digits.
+
+  if(String(seconds).indexOf('.') != -1) {
+
+    var beforeComma = String(seconds).split('.')[0]
+
+    var afterComma = String(seconds).split('.')[1].slice(0, 3)
+
+    seconds = beforeComma + afterComma
+  }
+
+  if(String(seconds).length < 4) {
+
+    while(String(seconds).length < 4) {
+
+      seconds = seconds * 10
+
+    }
+
+  }
+
+  return seconds
+
+}
+
