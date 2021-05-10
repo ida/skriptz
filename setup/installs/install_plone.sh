283d186170d93cbbf28244bb7279129ff0a3dd9b
user <user@example.com>
2021-05-10 11:42:11 +0200

    Merge scripts.

--- setup/installs/install_plone.sh
+++ setup/installs/install_plone.sh
@@ -1,3 +1,6 @@
+# For Plone-4.3.11 with LDAP-support:
+sudo apt-get install -y build-essential libjpeg-dev libldap2-dev libreadline-dev libsasl2-dev libssl-dev libxml2-dev libxslt1-dev libz-dev python-dev
+
 mkdir .buildout
 mkdir .buildout/eggs
 cat <<EOM > '.buildout/default.cfg'
--- setup/installs/install_syspckgs.sh
+++
@@ -1,3 +0,0 @@
-# For Plone-4.3.11 with LDAP-support:
-sudo apt-get install -y build-essential libjpeg-dev libldap2-dev libreadline-dev libsasl2-dev libssl-dev libxml2-dev libxslt1-dev libz-dev python-dev
-
