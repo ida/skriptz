23ff6ed0e49dc7a3d76f67d342118a6404203940
User <user@example.org>
2021-05-14 14:20:18 +0200

    up

--- setup/browser/ff/main.sh
+++ setup/browser/ff/main.sh
@@ -1,4 +1,6 @@
-addonPath='extensions/sharpcuts@example.org.xip'
+addonId=sharpcuts@example.org
+addonFileName=$addonId.xpi
+addonFilePath=extensions/$addonFileName
 
 profileName=ala
 
@@ -11,7 +13,7 @@ main() {
     setupProfile
     installAddon
     #firefox example.org -P $profileName
-# xip is missing
+# xpi is missing
 }
 
 
@@ -26,11 +28,19 @@ allowUnsignedAddons() {
 }
 
 copyAddonToAddonsFolder() {
-    test -f $profilePath/extensions/$addonPath && cp $addonPath $profilePath/extensions
+    test -f $profilePath/extensions/$addonFileName && log There is an addon-file in the addons-directory. || (
+        log Copying addon-file to addons-directory.
+        cp $addonFilePath $profilePath/extensions
+        log Copied addon-file to addons-directory.
+    )
 }
 
 createAddonsFolder() {
-    test ! -d $profilePath/extensions && mkdir $profilePath/extensions
+    test -d $profilePath/extensions && log There is an addons-directory in the profile-directory. || (
+        log Creating an addons-directory in the profile-directory.
+        mkdir $profilePath/extensions
+        log Created an addons-directory in the profile-directory.
+    )
 }
 
 createProfileDirectory() {
@@ -66,15 +76,24 @@ log() {
 }
 
 registerAddon() {
-    echo '{"schemaVersion":33,"addons":[{"id":"sharpcuts@example.org","syncGUID":"{717ca71f-ce17-4223-bf50-9c7f04b0f64d}","version":"0.1","type":"extension","loader":null,"updateURL":null,"optionsURL":null,"optionsType":null,"optionsBrowserStyle":true,"aboutURL":null,"defaultLocale":{"name":"sharpcuts","description":"Add custom shortcuts to every page.","creator":null,"developers":null,"translators":null,"contributors":null},"visible":true,"active":true,"userDisabled":false,"appDisabled":false,"embedderDisabled":false,"installDate":1620912254417,"updateDate":1620912383215,"applyBackgroundUpdates":1,"path":"'$profilePath'/extensions/sharpcuts@example.org.xpi","skinnable":false,"sourceURI":"file:///home/user/repos/github/ida/skriptz/setup/browser/ff/extensions/sharpcuts@example.org.xip","releaseNotesURI":null,"softDisabled":false,"foreignInstall":false,"strictCompatibility":true,"locales":[],"targetApplications":[{"id":"toolkit@mozilla.org","minVersion":null,"maxVersion":null}],"targetPlatforms":[],"signedState":0,"signedDate":null,"seen":true,"dependencies":[],"incognito":"spanning","userPermissions":{"permissions":[],"origins":["*://*/*"]},"optionalPermissions":{"permissions":[],"origins":[]},"icons":{},"iconURL":null,"blocklistState":0,"blocklistURL":null,"startupData":null,"hidden":false,"installTelemetryInfo":{"source":"about:addons","method":"install-from-file"},"recommendationState":null,"rootURI":"jar:file://'$profilePath'/extensions/sharpcuts@example.org.xpi!/","location":"app-profile"}]}' > $profilePath/extensions.json
-
-    echo 'user_pref("extensions.webextensions.uuids", "{\"sharpcuts@example.org\":\"9f708e6d-e513-4132-ac94-73ae6703c5a4\"}");' >> $profilePath/user.js
+    local path=$profilePath/extensions.json
+    (grep $addonId $path &> /dev/null) && log Addon is registered in extensions.json. || (
+        log Registering addon in $path.
+        echo '{"schemaVersion":33,"addons":[{"id":"sharpcuts@example.org","syncGUID":"{717ca71f-ce17-4223-bf50-9c7f04b0f64d}","version":"0.1","type":"extension","loader":null,"updateURL":null,"optionsURL":null,"optionsType":null,"optionsBrowserStyle":true,"aboutURL":null,"defaultLocale":{"name":"sharpcuts","description":"Add custom shortcuts to every page.","creator":null,"developers":null,"translators":null,"contributors":null},"visible":true,"active":true,"userDisabled":false,"appDisabled":false,"embedderDisabled":false,"installDate":1620912254417,"updateDate":1620912383215,"applyBackgroundUpdates":1,"path":"'$profilePath'/extensions/sharpcuts@example.org.xpi","skinnable":false,"sourceURI":"file:///home/user/repos/github/ida/skriptz/setup/browser/ff/extensions/sharpcuts@example.org.xip","releaseNotesURI":null,"softDisabled":false,"foreignInstall":false,"strictCompatibility":true,"locales":[],"targetApplications":[{"id":"toolkit@mozilla.org","minVersion":null,"maxVersion":null}],"targetPlatforms":[],"signedState":0,"signedDate":null,"seen":true,"dependencies":[],"incognito":"spanning","userPermissions":{"permissions":[],"origins":["*://*/*"]},"optionalPermissions":{"permissions":[],"origins":[]},"icons":{},"iconURL":null,"blocklistState":0,"blocklistURL":null,"startupData":null,"hidden":false,"installTelemetryInfo":{"source":"about:addons","method":"install-from-file"},"recommendationState":null,"rootURI":"jar:file://'$profilePath'/extensions/sharpcuts@example.org.xpi!/","location":"app-profile"}]}' > $path
+        log Registered addon in $path.
+    )
+    path=$profilePath/user.js
+    (grep $addonId $path &> /dev/null) && log Addon is registered in user.js. || (
+        log Registering addon in $path.
+        echo 'user_pref("extensions.webextensions.uuids", "{\"sharpcuts@example.org\":\"9f708e6d-e513-4132-ac94-73ae6703c5a4\"}");' >> $path
+        log Registered addon in $path.
+    )
 }
 
 setProfilePath() {
     # Assume find returns only one result:
     profilePath=$( find ~/.mozilla/firefox -name "*.$profileName" )
-    log Now profilePath is: $profilePath
+    log Set profilePath to: $profilePath
 }
 
 setupProfile() {
